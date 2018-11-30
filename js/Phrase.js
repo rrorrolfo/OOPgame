b668b8a4b40a44e182d6a1c762ec73f8b9f9a9fb
Rodolfo Perez <rodolfobeste@gmail.com>
2018-11-30 19:38:42 +0200

    Added keyboard functionality and checkLetter function progrmaming

--- js/Phrase.js
+++ js/Phrase.js
@@ -7,13 +7,17 @@ class Phrase {
     //adds letter placeholders to the display when the game starts.
     addPhraseToDisplay (phrase) {
         
+        //holds array of letters
         const a = [];
+        //holds list items with letters to display
         const b = [];
 
+        //Iteration for separating each phrase in single letters and add each letter to variable "a"
        for ( let i = 0; i < phrase.length; i += 1) {
         a.push(phrase.charAt(i));
        }
 
+       //Iteration for creating the list items that will hold each letter
        const c = a.map(letter => {
 
         if (letter !== " ") {
@@ -28,7 +32,27 @@ class Phrase {
 
     //checks to see if letter selected by player matches a letter in the phrase.
     checkLetter () {
+        
+            //hidden phrase to be compared against the chosen letter
+        const flag_phrase = document.querySelectorAll(".letter");
 
+
+        if (event.target.className === "key") {
+
+            // letter clicked on the keyboard
+            const flag_letter = event.target.textContent;
+
+            //Iteration over the hidden phrase
+            flag_phrase.forEach(letter => {
+
+                let letter_to_compare = letter.textContent;
+
+                //if the letters match, the letter will appear
+                if (letter_to_compare === flag_letter) {
+                    letter.className = "show letter";
+                }
+            });
+        }
     }
 
     //reveals the letter(s) on the board that matches player's selection.
--- js/app.js
+++ js/app.js
@@ -1,14 +1,19 @@
-// Hide layout when start button is clicked
+// VARIABLES
 
 const overlay = document.querySelector("#overlay");
 const start_btn = document.querySelector("#btn__reset");
-const phrase_to_guess = document.querySelector("#phrase")
+const phrase_to_guess = document.querySelector("#phrase ul");
+const keyboard = document.querySelector("#qwerty");
 
 let game = "";
 let game_phrase = ""; 
 
+//////// STARTING THE GAME ///////
+
+//event listener for start game button 
 start_btn.addEventListener("click", () => {
-    //Hides overlay
+
+    //Hides overlay when start button is clicked
     overlay.style.display = "none";
     //Creates new instance of a Game (game object)
     game = new Game();
@@ -16,8 +21,15 @@ start_btn.addEventListener("click", () => {
     game_phrase = new Phrase(game.getRandomPhrase());
     // Displays the hidden phrase to be guessed
     phrase_to_guess.innerHTML = game_phrase.addPhraseToDisplay(game_phrase.phrase);
+
 });
 
+/////// KEYBOARD FUNCTIONALITY ///////
 
+//event listener for the keyboard keys
+keyboard.addEventListener("click", (event) => {
+    
+    //checks if clicked letter on the keyboard is in the phrase
+    game_phrase.checkLetter();
 
-
+});
